d9c41a39b8064e874f5181371fe1979481a6aec5
Oslan Caio <caio.aguiar2528@gmail.com>
2021-06-08 21:14:44 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ const app = require('express');
 const cors = require('cors');
 const routes = require('./routes');
 
-server.use(routes);
 const server = app();
 
-
 server.use(cors());
+server.use(routes);
 
 server.listen(process.env.PORT || 3000,()=>{
   console.log('RODANDO');
