411c81c48c280351c1a87b5ad7be5055de346bb0
Oslan Caio <caio.aguiar2528@gmail.com>
2021-06-17 17:58:08 -0400

    Update sms.js

--- src/routes/sms.js
+++ src/routes/sms.js
@@ -1,13 +1,9 @@
 const app = require('express');
+require('dotenv').config();
 
 const routes = app.Router();
-//phone_number = "+5592988277815",
-const verify_service_sid = "VAe1839ae321b1d00c4d9eee53e6968d94",
-     // phone_number = "+5592991782441",
-      sid = "ACe8bca010069da860f8c120ee57551c7d",
-      token = "16bc675231b950bd0831121416e7a79b";
-
-const client = require('twilio')(sid, token);
+console.log(process.env.SID);
+const client = require('twilio')(process.env.SID, process.env.TOKEN);
 
 routes.get('/', (req, res)=>{
     return res.send('API');
@@ -18,23 +14,22 @@ routes.post('/send', (req, res)=>{
     console.log(number);
 
     client.verify
-    .services(verify_service_sid)
+    .services(process.env.VERIFY_SERVICE_SID)
     .verifications
     .create({
         to: number.toString(),
         channel: 'sms'
     }).then(response => 
         {console.log(response.status)
+            return res.send(response.status);
     })
     .catch(error => console.log(error))
-
-    return res.send('send');
 });
 
 routes.post('/verify', (req, res)=>{
     const { number, code } = req.body;
     client.verify
-    .services(verify_service_sid)
+    .services(process.env.VERIFY_SERVICE_SID)
       .verificationChecks
       .create({to: number, code})
       .then(verification_check => console.log(verification_check.status));
